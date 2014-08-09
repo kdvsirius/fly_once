8ed6a844b3078daad136f27b622c3e2d13c7b17e
Dmitriy kurlovich <kurlovichdv@mail.ru>
2014-08-09 20:21:30 +0400

    fix

--- src/index.js
+++ src/index.js
@@ -7,8 +7,17 @@ exports = module.exports = function(connect_options){
 		var client = redis.createClient(connect_options.port,connect_options.host);
 		return {
 				'once' : function(key,callback){
+						console.log(client);
+						try{
+								var application = currentApplication;
+						}
+						catch(err){
+								console.error('fly-sync current Application not defined');
+								var application = 'undefined';
+						}
+
+						key = application+'_'+key;
 						client.setnx(key,new Date().getTime(),function(err,val){
-								console.log(arguments);
 								if(val === 1){
 										callback(null);
 								}else{
